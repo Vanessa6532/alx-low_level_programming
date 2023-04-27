7eafaa31d1e4a871ab1fd06a77e8fe13c4a855d3
Vaness kinyuy <kinyuyvanessa45@gmail.com>
2023-04-26 17:50:53 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#includen "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+			
