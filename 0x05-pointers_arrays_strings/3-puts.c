8677654ce572d731570b898ff8108b1ec88e16d4
Vaness kinyuy <kinyuyvanessa45@gmail.com>
2023-05-02 19:29:20 -0700

    i do not need computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to be printed
@@ -8,8 +9,8 @@ void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+		putchar(*str);
 	}
-		_putchar('\n');
+		putchar('\n');
 }
 
