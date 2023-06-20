cd9d41cd65351d24c0ded914d92fa42fc7b92dc3
Vanessa6532 <kinyuyvanessa45@gmail.com>
2023-06-19 17:21:02 -0700

    preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - it is to  print the name of the file
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
